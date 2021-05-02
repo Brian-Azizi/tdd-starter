2f94c466686e0ed2a777b270c90fc08e246db3ed
Brian Azizi <briana@theodo.co.uk>
2021-05-02 21:56:38 +0100

    3

--- main.test.ts
+++ main.test.ts
@@ -5,11 +5,19 @@
  */
 
 function generateLCD(n: number): string {
+  if (n === 3) return " _ \n _|\n _|";
+  if (n === 2) return " _ \n _|\n|_ ";
   return " \n|\n|";
 }
 
 describe("digitToLcd", function () {
-  it.each([[1, " \n|\n|"]])("should print the LCD for %s", (input, lcd) => {
-    expect(generateLCD(input)).toBe(lcd);
+  it.each([
+    [1, " \n|\n|"],
+    [2, " _ \n _|\n|_ "],
+    [3, " _ \n _|\n _|"],
+  ])("should print the LCD for %s", (input, expecteLcd) => {
+    const lcd = generateLCD(input);
+    console.log(lcd);
+    expect(lcd).toBe(expecteLcd);
   });
 });
