5d1045764aba40a3bdf47f57d9fbaf6cea81553d
Brian Azizi <briana@theodo.co.uk>
2021-05-02 18:24:48 +0100

    Refactor

--- main.ts
+++ main.ts
@@ -3,12 +3,10 @@ export class PrimeFactors {
 
   public generate(n: number): number[] {
     let result = [];
-    while (n > 1) {
-      for (let p of this.PRIME_NUMBERS) {
-        while (n % p === 0) {
-          result.push(p);
-          n = n / p;
-        }
+    for (let p of this.PRIME_NUMBERS) {
+      while (n % p === 0) {
+        result.push(p);
+        n = n / p;
       }
     }
     return result;
