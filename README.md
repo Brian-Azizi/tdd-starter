fbb276f69b927f659e5651f1599a3373ff227b6c
Brian Azizi <briana@theodo.co.uk>
2021-05-02 14:22:31 +0100

    Init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tdd-starter
+
+A TypeScript starter to get you up and running for TDD Katas.
+
+```shell
+npm install
+npm test
+```
---
+++ main.test.ts
@@ -0,0 +1,7 @@
+import { add } from "./main";
+
+describe("add", function () {
+  it("should work", function () {
+    expect(add(1, 2)).toBe(3);
+  });
+});
