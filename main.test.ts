db764a93052eda64ac536d3662ce673b2f51df03
Brian Azizi <briana@theodo.co.uk>
2021-05-02 22:02:13 +0100

    6

--- main.test.ts
+++ main.test.ts
@@ -5,6 +5,9 @@
  */
 
 function generateLCD(n: number): string {
+  if (n === 6) return " _ \n|_ \n|_|";
+  if (n === 5) return " _ \n|_ \n _|";
+  if (n === 4) return "   \n|_|\n  |";
   if (n === 3) return " _ \n _|\n _|";
   if (n === 2) return " _ \n _|\n|_ ";
   return " \n|\n|";
@@ -15,6 +18,9 @@ describe("digitToLcd", function () {
     [1, " \n|\n|"],
     [2, " _ \n _|\n|_ "],
     [3, " _ \n _|\n _|"],
+    [4, "   \n|_|\n  |"],
+    [5, " _ \n|_ \n _|"],
+    [6, " _ \n|_ \n|_|"],
   ])("should print the LCD for %s", (input, expecteLcd) => {
     const lcd = generateLCD(input);
     console.log(lcd);
