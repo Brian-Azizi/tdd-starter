344f66df0c8e700d4b6e4b3b67027d930c9fc68b
Brian Azizi <briana@theodo.co.uk>
2021-05-02 18:45:20 +0100

    refactor

--- main.test.ts
+++ main.test.ts
@@ -29,27 +29,4 @@ describe("PrimeFactors", function () {
       expect(primeFactors.generate(n)).toStrictEqual(factors);
     });
   });
-
-  describe.only("generatePrimesUntil", () => {
-    it.each([
-      [1, []],
-      [2, [2]],
-      [3, [2, 3]],
-      [4, [2, 3]],
-      [5, [2, 3, 5]],
-      [6, [2, 3, 5]],
-      [7, [2, 3, 5, 7]],
-      [8, [2, 3, 5, 7]],
-      [9, [2, 3, 5, 7]],
-      [10, [2, 3, 5, 7]],
-      [11, [2, 3, 5, 7, 11]],
-      [20, [2, 3, 5, 7, 11, 13, 17, 19]],
-      // [14, [2, 7]],
-      // [15, [3, 5]],
-      // [25, [5, 5]],
-      // [106, [2, 53]],
-    ])("should return the list of primes until %s", (n, factors) => {
-      expect(primeFactors["generatePrimesUntil"](n)).toStrictEqual(factors);
-    });
-  });
 });
--- main.ts
+++ main.ts
@@ -1,43 +1,15 @@
 export class PrimeFactors {
-  PRIME_NUMBERS = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53];
-
-  private generatePrimesUntil(n: number): number[] {
-    let result = [];
-    if (n >= 2) {
-      result.push(2);
-    }
-    if (n >= 3) {
-      result.push(3);
-    }
-    if (n >= 5) {
-      result.push(5);
-    }
-    if (n >= 7) {
-      result.push(7);
-    }
-    if (n >= 11) {
-      result.push(11);
-    }
-    if (n >= 13) {
-      result.push(13);
-    }
-    if (n >= 17) {
-      result.push(17);
-    }
-    if (n >= 19) {
-      result.push(19);
-    }
-    return result;
-  }
-
   public generate(n: number): number[] {
     let result = [];
-    for (let p of this.generatePrimesUntil(n)) {
-      while (n % p === 0) {
-        result.push(p);
-        n = n / p;
+    for (let i = 2; i <= n; i++) {
+      if (n >= i) {
+        while (n % i === 0) {
+          result.push(i);
+          n = n / i;
+        }
       }
     }
+
     return result;
   }
 }
