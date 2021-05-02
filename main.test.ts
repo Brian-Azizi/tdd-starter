8bcd9b79b965c1c65529e621a0e4d81720fa7875
Brian Azizi <briana@theodo.co.uk>
2021-05-02 18:07:56 +0100

    red

--- main.test.ts
+++ main.test.ts
@@ -16,7 +16,12 @@ describe("PrimeFactors", function () {
       [4, [2, 2]],
       [5, [5]],
       [6, [2, 3]],
+      [7, [7]],
+      [9, [3, 3]],
       [10, [2, 5]],
+      [14, [2, 7]],
+      [15, [3, 5]],
+      [25, [5, 5]],
     ])(
       "should return the prime factor decomposition when the input is %n",
       (n, factors) => {
--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ export class PrimeFactors {
     if (n > 2 && n % 2 === 0) {
       return [2, n / 2];
     }
+    if (n > 3 && n % 3 === 0) {
+      return [3, n / 3];
+    }
     return [n];
   }
 }
