41a04e795de4ac7c18edc1c235449e422adbf575
Brian Azizi <briana@theodo.co.uk>
2021-05-02 22:11:55 +0100

    Green digits

--- main.test.ts
+++ main.test.ts
@@ -5,15 +5,23 @@
  */
 
 enum LCD {
+  ZERO = " _ \n| |\n|_|",
   ONE = " \n|\n|",
   TWO = " _ \n _|\n|_ ",
   THREE = " _ \n _|\n _|",
   FOUR = "   \n|_|\n  |",
   FIVE = " _ \n|_ \n _|",
   SIX = " _ \n|_ \n|_|",
+  SEVEN = " _ \n  |\n  |",
+  EIGHT = " _ \n|_|\n|_|",
+  NINE = " _ \n|_|\n _|",
 }
 
 function generateLCD(n: number): string {
+  if (n === 0) return LCD.ZERO;
+  if (n === 9) return LCD.NINE;
+  if (n === 8) return LCD.EIGHT;
+  if (n === 7) return LCD.SEVEN;
   if (n === 6) return LCD.SIX;
   if (n === 5) return LCD.FIVE;
   if (n === 4) return LCD.FOUR;
@@ -24,12 +32,16 @@ function generateLCD(n: number): string {
 
 describe("digitToLcd", function () {
   it.each([
+    [0, " _ \n| |\n|_|"],
     [1, " \n|\n|"],
     [2, " _ \n _|\n|_ "],
     [3, " _ \n _|\n _|"],
     [4, "   \n|_|\n  |"],
     [5, " _ \n|_ \n _|"],
     [6, " _ \n|_ \n|_|"],
+    [7, " _ \n  |\n  |"],
+    [8, " _ \n|_|\n|_|"],
+    [9, " _ \n|_|\n _|"],
   ])("should print the LCD for %s", (input, expecteLcd) => {
     const lcd = generateLCD(input);
     console.log(lcd);
