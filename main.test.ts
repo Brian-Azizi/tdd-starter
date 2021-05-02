e493f01eff22d2ce9f9f1abd939f72d0dab5b081
Brian Azizi <briana@theodo.co.uk>
2021-05-02 18:34:53 +0100

    Green (only)

--- main.test.ts
+++ main.test.ts
@@ -29,4 +29,25 @@ describe("PrimeFactors", function () {
       expect(primeFactors.generate(n)).toStrictEqual(factors);
     });
   });
+
+  describe.only("generatePrimesUntil", () => {
+    it.each([
+      [2, [2]],
+      [3, [2, 3]],
+      [4, [2, 3]],
+      [5, [2, 3, 5]],
+      [6, [2, 3, 5]],
+      [7, [2, 3, 5, 7]],
+      // [8, [2, 2, 2]],
+      // [9, [3, 3]],
+      // [10, [2, 5]],
+      // [11, [11]],
+      // [14, [2, 7]],
+      // [15, [3, 5]],
+      // [25, [5, 5]],
+      // [106, [2, 53]],
+    ])("should return the list of primes until %s", (n, factors) => {
+      expect(primeFactors["generatePrimesUntil"](n)).toStrictEqual(factors);
+    });
+  });
 });
--- main.ts
+++ main.ts
@@ -1,9 +1,23 @@
 export class PrimeFactors {
   PRIME_NUMBERS = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53];
 
+  private generatePrimesUntil(n: number): number[] {
+    let result = [2];
+    if (n >= 3) {
+      result.push(3);
+    }
+    if (n >= 5) {
+      result.push(5);
+    }
+    if (n >= 7) {
+      result.push(7);
+    }
+    return result;
+  }
+
   public generate(n: number): number[] {
     let result = [];
-    for (let p of this.PRIME_NUMBERS) {
+    for (let p of this.generatePrimesUntil(n)) {
       while (n % p === 0) {
         result.push(p);
         n = n / p;
